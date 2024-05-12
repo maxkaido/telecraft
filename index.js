95cd75d89730610ee3e226cb4c5fd881849338fb
Max Kaido <maximkaido@gmail.com>
2024-05-13 00:44:29 +0300

    cleanup

--- index.js
+++
@@ -1,6 +0,0 @@
-import store from "./store.js";
-import { initServer, initDatabase, initBot } from "./actions/index.js";
-
-store.dispatch(initServer());
-store.dispatch(initDatabase());
-store.dispatch(initBot());
